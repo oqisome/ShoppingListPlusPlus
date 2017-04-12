deb71dfcf6c421e7ea2577b8717c29aca3aeef32
oqisome <oqisome@users.noreply.github.com>
2017-04-12 14:53:26 +0200

    practice pull

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.java
@@ -10,9 +10,9 @@ public class ShoppingListApplication extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        /* Initialize Firebase */
-        //In the new SDK, it's no longer necessary to call Firebase.setAndroidContext() so you can remove it from your code.
-        //Firebase.setAndroidContext(this);
+        /* Initialize Firebase
+        In the new SDK, it's no longer necessary to call Firebase.setAndroidContext() so you can remove it from your code.
+        Firebase.setAndroidContext(this);*/
     }
 
 }
